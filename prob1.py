e2bb60993f0e0b7891b02947b15ec5606c66fbf0
Tasmiah A. <154039529+mieverse@users.noreply.github.com>
2025-05-26 08:29:47 +0600

    Delete prob1.py

--- prob1.py
+++ prob1.py
@@ -1,18 +1,3 @@
 # 1
 
-def result(lis, k):
-  p = 0
-  q = len(lis)  #4
-  for i in range(0,q):
-    p=i+1
-    while p <= (q-1): #3
-      sum = lis[i] + lis[p]
-      p+=1
-      if sum == k:
-        return True
-      else:
-        return False
 
-a = [10, 15, 3, 7]
-b = 16
-result(a,b)
